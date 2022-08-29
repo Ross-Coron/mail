9f8b02ba01d6f3c3bb6168915eac39f70ca395eb
Ross P. Coron <rpcoron@gmail.com>
2022-08-29 17:18:39 +0100

    Updated load_mailbox function
    Additional comments also.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,21 +1,23 @@
+// TODO: styling, comment, archive email option
+
 document.addEventListener('DOMContentLoaded', function() {
 
-  // By default, load the inbox
+  // By default, load the inbox TODO: duplicate?
   load_mailbox('inbox');
 
-  // Event listeners for each button. Use buttons to toggle between views
+  // Event listeners for each NavBar button. Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
-
-  // Test function - alert and console log form field
+  // When compose email form submitted
   document.querySelector('#compose-form').onsubmit = function() {
 
-    // By default, load the inbox
+    // By default, load the inbox TODO: duplicate?
     load_mailbox('inbox');
 
+    // Send email via POST request to /emails.
     fetch('/emails', {
         method: 'POST',
         body: JSON.stringify({
@@ -25,15 +27,15 @@ document.addEventListener('DOMContentLoaded', function() {
         })
       })
 
+      // ???
       .then(response => response.json())
       .then(result => {
-        // Print result
+        // Debug
         console.log(result);
       })
 
-    // Prevent form from submitting
+    // Prevents form from submitting
     return false;
-
   };
 });
 
@@ -44,16 +46,16 @@ function compose_email() {
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
-  // Clear out composition fields
+  // Clear out any previous values from form fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
 }
 
-// Three mailbox arguments: inbox, sent, and archive
+// Called by clicking NavBar button. Three valid arguments: inbox, sent, and archive
 function load_mailbox(mailbox) {
 
-  // Show the mailbox and hide other views
+  // Show mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#email-view').style.display = 'none';
@@ -65,22 +67,36 @@ function load_mailbox(mailbox) {
     // Debug
     console.log("Inbox - success")
 
+    // GET request to emails/inbox, converts to JSON, provide array of emails as variable emails
     fetch('/emails/inbox')
       .then(response => response.json())
-      .then(email => {
+      .then(emails => {
         // Debug
-        console.log(email);
+        console.log(emails);
 
-        for (const x in email) {
+        // For each email, create a list item with styling
+        for (const email in emails) {
           var list_item = document.createElement("li");
           list_item.id = "style_test";
 
-          list_item.innerHTML = `Email from ${email[x].sender} recieved ${email[x].timestamp} <button class="view" data-email="${email[x].id}">View</button>`;
+          list_item.innerHTML = `Email from ${emails[email].sender} recieved ${emails[email].timestamp} <button class="view" data-email="${emails[email].id}">View</button>`;
+
+          // If email has been read (default is unread), render in grey
+          if (emails[email].read === true) {
+            console.log("Read");
+            list_item.style.backgroundColor = "gray";
+
+          } else {
+            console.log("Not read")
+            list_item.style.backgroundColor = "white";
+          }
+
+          // Add populated list item to page
           document.querySelector('#emails-view').appendChild(list_item);
         }
       });
 
-    // If hide button is clicked, delete the post
+    // ???
     document.addEventListener('click', event => {
 
       // Find what was clicked on
@@ -89,9 +105,7 @@ function load_mailbox(mailbox) {
       // Check if the user clicked the view button
       if (element.className === 'view') {
         view_email(event.target.dataset.email)
-
       }
-
     });
 
     // Test alerts for mailbox specification
@@ -113,6 +127,13 @@ function view_email(email) {
   console.log(event.target.dataset.email)
   email = event.target.dataset.email
 
+  fetch(`/emails/${email}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      read: true
+    })
+  });
+
   fetch(`/emails/${email}`)
     .then(response => response.json())
     .then(email => {
@@ -120,11 +141,13 @@ function view_email(email) {
       console.log(email);
 
       // For fields in eamil, create an list item and display value
+      document.getElementById("email-view").innerHTML = '';
+
       for (const field in email) {
         var list_item = document.createElement("li");
-        list_item.id = "test";
-        list_item.innerHTML = email[field];
+
+        list_item.innerHTML = `${field}: ${email[field]}`;
         document.querySelector('#email-view').appendChild(list_item);
       }
-    });
+    })
 }
--- mail/urls.py
+++ mail/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="index"),    # Default route
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
@@ -11,5 +11,5 @@ urlpatterns = [
     # API Routes
     path("emails", views.compose, name="compose"),
     path("emails/<int:email_id>", views.email, name="email"),
-    path("emails/<str:mailbox>", views.mailbox, name="mailbox"),
+    path("emails/<str:mailbox>", views.mailbox, name="mailbox"),    # GET request where mailbox is inbox, sent, or archive. Returns list of emails.
 ]
